ce2c98442c6195d7b0828c4cbb9739a773840f5b
Nikita <nikita.kruk1999@gmail.com>
2022-11-15 11:45:57 +0300

    HomeWork_Lesson_3_1

--- Lesson3/src/main/java/HomeWork.java
+++ Lesson3/src/main/java/HomeWork.java
@@ -8,41 +8,51 @@ public class HomeWork {
         System.out.println(calculateHypotenuse(12, 16));
     }
 
+    /**
+     * 1. Метод должен вернуть сумму двух чисел a и b
+     * 2. Дополнительно: сделать проверку если сумма a и b больше чем максимальное значение int то вернуть -1
+     **/
     public static int sum(int a, int b) {
-        if ((a + b) > Integer.MAX_VALUE) {
+        if ((long) a + b > Integer.MAX_VALUE) {
             return -1;
-        } else {
-            return a + b;
-
         }
+        return a + b;
     }
+
+    // Метод должен вернуть максимальное значение из двух чисел
 
     public static int max(int a, int b) {
         return Math.max(a, b);
     }
 
+    // Метод должен вернуть среднее значение из массива чисел
+
     public static double average(int[] array) {
-        int average = array[1];
-        for (int j : array) {
-            average += j / array.length;
+        double average = 0;
+        for (int value : array) {
+            average += value;
         }
-        return average;
+        return average / array.length;
     }
 
+    /**
+     * Метод должен вернуть максимальый элемент массива. Пример: array = {1,2,10,3} метод возвращает 10
+     **/
     public static int max(int[] array) {
-        int max = array[0];
-        for (int currentValue : array) {
-            if (currentValue > max) {
-                max = currentValue;
+        if (array != null && array.length > 0) {
+            int max = array[0];
+            for (int currentValue : array) {
+                if (currentValue > max) {
+                    max = currentValue;
+                }
             }
+            return max;
         }
-
-        return max;
+        return 0;
     }
 
     public static double calculateHypotenuse(int a, int b) {
         int c = (int) (Math.pow(a, 2) + Math.pow(b, 2));
-
         return (int) Math.sqrt(c);
     }
 }
