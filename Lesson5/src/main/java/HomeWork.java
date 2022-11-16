a5a17c5098e86db7c998f6a5ed96bba80dd339db
Nikita <nikita.kruk1999@gmail.com>
2022-11-16 14:16:03 +0300

    HomeWork_Lesson_5_3

--- Lesson5/src/main/java/HomeWork.java
+++ Lesson5/src/main/java/HomeWork.java
@@ -5,7 +5,8 @@ public class HomeWork {
     public static void main(String[] args) {
         //dayOfTheWeek();
         //amoeba();
-        numberOfDigits();
+        //numberOfDigits();
+        //System.out.println (getZodiacName(7,15));
     }
 
 
@@ -48,18 +49,51 @@ public class HomeWork {
         while (true) {
             Scanner scanner = new Scanner(System.in);
             System.out.print("Введите число: ");
-            int value = scanner.nextInt();
-            int i = (int) Math.ceil(Math.log10(Math.abs(value)));
-            if (value > 0) {
-                System.out.println(value + " - это положительное число, количество цифр = " + i);
+            int number = scanner.nextInt();
+            int count = (int) Math.ceil(Math.log10(Math.abs(number)));
+            if (number > 0) {
+                System.out.println(number + " - это положительное число, количество цифр = " + count);
                 break;
-            } else if (value < 0) {
-                System.out.println(value + " - это отрицательное число, количество цифр = " + i);
+            } else if (number < 0) {
+                System.out.println(number + " - это отрицательное число, количество цифр = " + count);
                 break;
             } else {
                 System.out.println("Ошибка");
                 continue;
             }
         }
+    }
+    //4) Дано 2 числа, день и месяц рождения. Написать программу, которая определяет знак зодиака человека.
+
+    public static String getZodiacName(int month, int day) {
+        String result = null;
+        if (month == 1 && day >= 21 && day <= 31 || month == 2 && day >= 1 && day <= 19) {
+            result = "Водолей";
+        } else if (month == 2 && day >= 20 && day <= 29 || month == 3 && day >= 1 && day <= 20) {
+            result = "Рыбы";
+        } else if (month == 3 && day >= 21 && day <= 31 || month == 4 && day >= 1 && day <= 20) {
+            result = "Овен";
+        } else if (month == 4 && day >= 21 && day <= 30 || month == 5 && day >= 1 && day <= 21) {
+            result = "Телец";
+        } else if (month == 5 && day >= 22 && day <= 31 || month == 6 && day >= 1 && day <= 21) {
+            result = "Близнецы";
+        } else if (month == 6 && day >= 22 && day <= 30 || month == 7 && day >= 1 && day <= 22) {
+            result = "Рак";
+        } else if (month == 7 && day >= 23 && day <= 31 || month == 8 && day >= 1 && day <= 21) {
+            result = "Лев";
+        } else if (month == 8 && day >= 22 && day <= 31 || month == 9 && day >= 1 && day <= 23) {
+            result = "Дева";
+        } else if (month == 9 && day >= 24 && day <= 30 || month == 10 && day >= 1 && day <= 23) {
+            result = "Весы";
+        } else if (month == 10 && day >= 24 && day <= 31 || month == 11 && day >= 1 && day <= 23) {
+            result = "Скорпион";
+        } else if (month == 11 && day >= 24 && day <= 30 || month == 12 && day >= 1 && day <= 22) {
+            result = "Стрелец";
+        } else if (month == 12 && day >= 23 && day <= 31 || month == 1 && day >= 1 && day <= 20) {
+            result = "Козерог";
+        } else {
+            result = "Введены неправильные данные";
+        }
+        return result;
     }
 }
