96b3307cfee05f8bcaffe8647ec163073fa60737
Nikita <nikita.kruk1999@gmail.com>
2022-11-16 11:27:40 +0300

    HomeWork_Lesson_5_1

--- Lesson5/src/main/java/HomeWork.java
+++ Lesson5/src/main/java/HomeWork.java
@@ -2,7 +2,8 @@ import java.util.Random;
 
 public class HomeWork {
     public static void main(String[] args) {
-        dayOfTheWeek();
+        //dayOfTheWeek();
+        amoeba();
     }
 
 
@@ -10,12 +11,11 @@ public class HomeWork {
 //        Рандомно генерируется число От 1 до 7.
 //        Если число равно 1, выводим на консоль “Понедельник”, 2 –”Вторник” и так далее.
 //        Если 6 или 7 – “Выходной”.
+
     private static void dayOfTheWeek() {
-        int max = 8;
-        int min = 1;
         Random random = new Random();
-        int randomPeriod = random.nextInt(max - min) + min;
-        //System.out.println(randomPeriod);
+        int randomPeriod = random.nextInt(8 - 1) + 1;
+        // System.out.println(randomPeriod);
         switch (randomPeriod) {
             case (1) -> System.out.println("Понедельник");
             case (2) -> System.out.println("Вторник");
@@ -23,6 +23,18 @@ public class HomeWork {
             case (4) -> System.out.println("Четверг");
             case (5) -> System.out.println("пятница");
             default -> System.out.println("Выходной");
+        }
+    }
+    //Одноклеточная амеба каждые 3 часа делится на 2 клетки. Определить,
+//         сколько амеб будет через 3, 6, 9, 12,..., 24 часа
+
+    private static void amoeba() {
+        int numberOfAmoebas = 1;
+        int hours = 0;
+        for (int i = 0; i < 8; i++) {
+            numberOfAmoebas = numberOfAmoebas * 2;
+            hours += 3;
+            System.out.println(numberOfAmoebas + " амёбы через " + hours + " часов");
         }
     }
 }
